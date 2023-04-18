cd639daee16a48a05390741fe812fd0aa940c955
Sandhya80 <129001012+Sandhya80@users.noreply.github.com>
2023-04-18 23:48:14 +0100

    Initial commit

---
+++ DogYears.js
@@ -0,0 +1,22 @@
+//My age
+const myAge = 42;
+//Dog's early years
+let earlyYears = 2;
+//multiplying the value by 10.5 using multiplication assignment operator
+earlyYears *= 10.5;
+//subtracting 2 from my Age
+let laterYears = myAge - 2;
+//Dog years based on later Years
+laterYears *= 4;
+//Check early Years and later Years
+console.log(earlyYears);
+console.log(laterYears);
+//Calculating my age in Dog Years
+myAgeInDogYears = earlyYears + laterYears;
+//My name as a string in all lowercase
+myName = 'Sandhya'.toLowerCase();
+//My name with my human age and my Dog age
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
+
